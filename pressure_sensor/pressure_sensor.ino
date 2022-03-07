23a39a5809d26221c2da8e40a0cdeef984c5d96a
Heinz <a17014877@alumnos.uady.mx>
2022-03-07 14:37:35 -0600

    código casi termindo, sólo faltaría el método de conversión de la batería y del voltaje del trans

--- pressure_sensor/pressure_sensor.ino
+++ pressure_sensor/pressure_sensor.ino
@@ -1,7 +1,9 @@
 #include <SD.h>
 #include <SPI.h>
+#include <Sodaq_PcInt.h>
 #include <Sodaq_DS3231.h>
 #include <LowPower.h>
+#define DEBUG
 
 //switched power lines pin
 #define SWITCHED_POWER 22
@@ -14,7 +16,7 @@ bool error;
 #define SUCCESS_LED 8
 bool success;
 //SD card pin 
-#define sd= 10;
+#define sd 10
 //DateTime variable for the time
 DateTime now;
 
@@ -31,20 +33,22 @@ float batteryvoltage;       // the battery voltage as calculated by the formula 
 //pin for the RTC alarm interrupt
 int interruptPin = A7;
 //pin for the pressure sensor
-#define transductor_voltage = A2;
+#define transductor_voltage A2
 // function for converting the voltage values into pressure values
-float pressure(volt){
+//float pressure(){
   ///////////
   //work in progress
   ////////
-}
+//}
 // Interrupt service routine for RTC alarm
 void INT0_ISR()
 {
   //nothig here, ,just an interrupt to awake the device
 }
 void setup(){
-  //Serial.begin(9600);
+  #if defined DEBUG
+  Serial.begin(9600);
+  #endif
   //configure switched power, error and success pins
     pinMode(SWITCHED_POWER, OUTPUT);
     digitalWrite(SWITCHED_POWER, LOW);
@@ -53,10 +57,18 @@ void setup(){
     pinMode(SUCCESS_LED, OUTPUT);
     digitalWrite(SUCCESS_LED, LOW);
     //If the SD card doesn't initialize 
-    while (!SD.begin(sd)) {
+     while(!SD.begin(sd)) {
+      #if defined DEBUG
+        Serial.print("SD card reding failed");
+      #endif
       digitalWrite(ERROR_LED, HIGH);
       delay(500);//espera antes de volver a intentarlo 
     }
+    digitalWrite(ERROR_LED, LOW);
+    #if defined DEBUG
+      Serial.print("SD card reading succesful");
+    #endif
+    
     //Configure the interrupt to awake the device
     pinMode(interruptPin, INPUT_PULLUP);
     PcInt::attachInterrupt(interruptPin, INT0_ISR);
@@ -66,6 +78,57 @@ void setup(){
 
 }
 void loop(){
-  
+  //turn the switched source on
+    digitalWrite(SWITCHED_POWER, HIGH);
+    float volt = analogRead(transductor_voltage);
+    //power sensor off as soon as it finishes reading
+    digitalWrite(SWITCHED_POWER, LOW);
+    string_buffer = "";
+    batterysenseValue = analogRead(batteryPin); 
+    ////////
+    ///waiting for verification
+    batteryvoltage = (3.3/1023.) * 4.7037 * batterysenseValue;
+    ///
+    //////
+    now = rtc.now();
+    //
+    string_buffer = String(now.date()) + "_" + String(now.month()) + ".csv";
+
+    // section to verify if the file exists, if it doesn't then create it and generate the text file header
+    if (!SD.exists(string_buffer))
+    {
+      file = SD.open(string_buffer, FILE_WRITE);
+      if (file)
+      {
+        file.println(F("Fecha/hora, presión, Bateria"));
+        file.close();
+      }
+      else error = true;
+    }
+    file = SD.open(string_buffer, FILE_WRITE);
+    string_buffer = ""; 
+    now.addToString(string_buffer);
+    string_buffer.concat("," + String(volt)+","+ String(batteryvoltage));
+    #if defined DEBUG
+    Serial.println(string_buffer);
+    #endif
+    if (file)
+    {
+      file.println(string_buffer);
+      file.close();
+      #if defined DEBUG
+      Serial.println(F("Succesfully written to the SD"));
+      #endif
+      error = false;
+    }
+    else error = true;
+    //update error pin status
+    if (error){
+      digitalWrite(ERROR_LED, HIGH);
+    }
+     //This section clears the alarm flag of the RTC and puts the device in deep sleep
+    rtc.clearINTStatus();
+    LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+
 
 }
